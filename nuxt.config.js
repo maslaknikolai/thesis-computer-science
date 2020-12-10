d34dd973f7fd5d99d1264ec339a8f19d24d3cca8
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2020-12-11 02:07:44 +0300

    Заголовок

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - thesis-computer-science',
-    title: 'thesis-computer-science',
+    titleTemplate: '%s - Информатика',
+    title: 'Информатика',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
